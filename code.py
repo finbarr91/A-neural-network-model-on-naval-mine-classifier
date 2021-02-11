a573f4fb50582fbfd8b0eec113e889a2279ac7fc
Obierefu Finbarr Chukwuka <57522378+finbarr91@users.noreply.github.com>
2021-02-11 13:20:47 -0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,2 @@
+# A-neural-network-model-on-naval-mine-classifier
+This is a neural network model that classisfes sonar signals. It is written in Python, tensorflow and Keras. The task is to train a network to discriminate between sonar signals bounced off a metal cylinder and those bounced off a roughly cylindrical rock.
